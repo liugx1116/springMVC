b80117c2cc86f8afb6aeef7bc120d9cf9a681b7f
liugx <sunshine_lgx@163.com>
2022-03-06 22:34:50 +0800

    联系

--- springMVC-demon04/src/main/java/com/atgx/mvc/controller/HttpController.java
+++ springMVC-demon04/src/main/java/com/atgx/mvc/controller/HttpController.java
@@ -1,8 +1,14 @@
 package com.atgx.mvc.controller;
 
+import com.atgx.mvc.bean.User;
+import org.springframework.http.RequestEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class HttpController {
@@ -10,5 +16,35 @@ public class HttpController {
     public String testRequestBody(@RequestBody String requestBody){
         System.out.println("testRequestBody:"+requestBody);
         return "success";
+    }
+
+    @RequestMapping("/testRequestEntity")
+    public String testRequestEntity(RequestEntity<String> requestMapping){
+        System.out.println("请求头："+requestMapping.getHeaders());
+        System.out.println("请求体："+requestMapping.getBody());
+        return "success";
+    }
+
+    @RequestMapping("/testResponseBody")
+    @ResponseBody
+    public String testResponseBody(){
+        return "success";
+    }
+
+    @RequestMapping("/testResponse")
+    public void testResponse(HttpServletResponse response) throws IOException {
+        response.getWriter().println("hello,response");
+    }
+    @RequestMapping("/testResponseUser")
+    @ResponseBody
+    public User testResponseUser(){
+        return new User(1001,"admin","123",23,"男");
+    }
+
+    @RequestMapping("/testAxios")
+    @ResponseBody
+    public String testAxios(String username,String password){
+        System.out.println("username:"+username+"password:"+password);
+        return "hello,ajax";
     }
 }
